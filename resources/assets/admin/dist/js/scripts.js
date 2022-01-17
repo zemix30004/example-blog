3cdffd23003827cc2c007fece52be4b9329baebb
zemix30004 <eugenezm@gmail.com>
2022-01-17 19:56:36 +0200

    end tuning webpack css js

---
+++ resources/assets/admin/dist/js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+$("#example1").DataTable();
+$(".select2").select2();
+//Date picker
+$('#datepicker').datepicker({
+  autoclose: true
+});
+//iCheck for checkbox and radio inputs
+$('input[type="checkbox"].minimal, input[type="radio"].minimal').iCheck({
+  checkboxClass: 'icheckbox_minimal-blue',
+  radioClass: 'iradio_minimal-blue'
+});
+});
